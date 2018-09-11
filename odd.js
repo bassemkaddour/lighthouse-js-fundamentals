917f056bef5f27d63a7486f073841babba75658f
Bassem Kaddour <bassemkaddour@gmail.com>
2018-09-11 04:40:29 +0000

    Updated odd.js

--- odd.js
+++ odd.js
@@ -4,5 +4,3 @@ function isOdd(num) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
-console.log("-39 is odd: " + isOdd(-39));
-console.log("-38 is odd: " + isOdd(-38));
